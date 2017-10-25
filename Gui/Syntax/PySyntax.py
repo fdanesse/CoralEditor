024fdaba8accfdc7f1c00e1327927f9b4b83e88b
flavio <fdanesse@gmail.com>
2017-10-24 22:08:06 -0300

    Simplificación en resaltado de código

--- Gui/Syntax/PySyntax.py
+++ Gui/Syntax/PySyntax.py
@@ -20,35 +20,21 @@ def __format(color, style=''):
         _format.setFontItalic(True)
     return _format
 
-
-COLOR_SCHEME = {
-    "keyword": "#c9264c",
-    "operator": "#c9264c",
-    "definition": "#a4de2d",
-    "definition2": "cyan",
-    "string": "#d8d170",
-    "string2": "#ff9dc6",
-    "comment": "gray",
-    "properObject": "orange",
-    "numbers": "brown",
-    }
-
 STYLES = {
-    'keyword': __format(COLOR_SCHEME['keyword']),
-    'operator': __format(COLOR_SCHEME['operator']),
-    'definition': __format(COLOR_SCHEME['definition']),
-    'definition2': __format(COLOR_SCHEME['definition2']),
-    'string': __format(COLOR_SCHEME['string']),
-    'string2': __format(COLOR_SCHEME['string2']),
-    'comment': __format(COLOR_SCHEME['comment']),
-    'properObject': __format(COLOR_SCHEME['properObject']),
-    'numbers': __format(COLOR_SCHEME['numbers']),
+    'keyword': __format("#c9264c"),
+    'operator': __format("#c9264c"),
+    'definition': __format("#a4de2d"),
+    'definition2': __format("cyan"),
+    'string': __format("#d8d170"),
+    'string2': __format("#ff9dc6"),
+    'comment': __format("gray"),
+    'properObject': __format("orange"),
+    'numbers': __format("brown"),
     }
 
 SYNTAX = {
     'comment': ['#'],
     'definition': ['def', 'class', 'super'],
-    'definition2': ['def', 'class', 'super'],
     'string': ["'", '"'],
     'properObject': ['self'],
     'operators': ['=', '==', '!=', '<', '<=', '>', '>=', '\\+', '-', '\\*',
@@ -65,12 +51,12 @@ tri_single = (QRegExp("'''"), 1, STYLES['string2'])
 tri_double = (QRegExp('"""'), 2, STYLES['string2'])
 
 RULES = []
-RULES.extend([(r'\b%s\b' % w, 0, STYLES['keyword']) for w in SYNTAX.get('keywords', [])])
-RULES.extend([(r'%s' % w, 0, STYLES['operator']) for w in SYNTAX.get('operators', [])])
-RULES.extend([(r'\b%s\b' % w, 0, STYLES['properObject']) for w in SYNTAX.get('properObject', [])])
-#Nombres de clases o función
-RULES.extend([(r'\b%s\b\s*(\w+)' % w, 1, STYLES['definition']) for w in SYNTAX.get('definition', [])])
-RULES.extend([(r'\b%s\b\s' % w, 0, STYLES['definition2']) for w in SYNTAX.get('definition2', [])])
+RULES.extend([(r'\b%s\b' % w, 0, STYLES['keyword']) for w in SYNTAX['keywords']])
+RULES.extend([(r'%s' % w, 0, STYLES['operator']) for w in SYNTAX['operators']])
+RULES.extend([(r'\b%s\b' % w, 0, STYLES['properObject']) for w in SYNTAX['properObject']])
+#Nombres de clase o función
+RULES.extend([(r'\b%s\b\s*(\w+)' % w, 1, STYLES['definition']) for w in SYNTAX['definition']])
+RULES.extend([(r'\b%s\b\s' % w, 0, STYLES['definition2']) for w in SYNTAX['definition']])
 
 # Numeric literals # r fuerza a que las secuencias de escape no sean interpretadas
 RULES.extend([
